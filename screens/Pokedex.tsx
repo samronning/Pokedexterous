879d245328fe5735f87ba568565e9c1fbaf26122
Sam Ronning <samronning@gmail.com>
2022-12-31 17:29:56 -0500

    add debounce to search

--- screens/Pokedex.tsx
+++ screens/Pokedex.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Dispatch } from "react";
 import { View, Text } from "react-native";
 import EntryRender from "../components/Pokedex/EntryRender";
 import { PokedexEntry } from "../components/Pokedex/Entry";
@@ -9,40 +9,47 @@ import readSqlFile from "../sql/readSql";
 import { selectGeneration } from "../slices/generation";
 import { selectSearch } from "../slices/search";
 import { useAppSelector } from "../hooks/redux";
+import { debounce } from "lodash";
+
+const doQuery = async (
+  selectedGeneration: number | string,
+  searchTerm: string,
+  setPokedexEntries: Dispatch<PokedexEntry[]>
+) => {
+  try {
+    console.log("before await");
+    const db = await openDatabase();
+    const sqlStatement = await readSqlFile("selectPokedex");
+    db.transaction(
+      (sqlTransaction: SQLTransaction) => {
+        sqlTransaction.executeSql(
+          sqlStatement,
+          [selectedGeneration, selectedGeneration, searchTerm],
+          (_, res) => {
+            setPokedexEntries(res.rows._array);
+          }
+        );
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        console.log("success");
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const debouncedDoQuery = debounce(doQuery, 800);
 
 const Pokedex = () => {
   const [pokedexEntries, setPokedexEntries] = useState<PokedexEntry[]>([]);
   const selectedGeneration = useAppSelector(selectGeneration);
   const searchTerm = useAppSelector(selectSearch);
   useEffect(() => {
-    console.log(searchTerm);
-    const doQuery = async () => {
-      try {
-        console.log("before await");
-        const db = await openDatabase();
-        const sqlStatement = await readSqlFile("selectPokedex");
-        db.transaction(
-          (sqlTransaction: SQLTransaction) => {
-            sqlTransaction.executeSql(
-              sqlStatement,
-              [selectedGeneration, selectedGeneration, searchTerm],
-              (_, res) => {
-                setPokedexEntries(res.rows._array);
-              }
-            );
-          },
-          (err) => {
-            console.log(err);
-          },
-          () => {
-            console.log("success");
-          }
-        );
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    doQuery();
+    debouncedDoQuery(selectedGeneration, searchTerm, setPokedexEntries);
   }, [selectedGeneration, searchTerm]);
   return (
     <View
